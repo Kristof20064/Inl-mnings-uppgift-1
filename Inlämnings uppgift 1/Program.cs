78cda2d21abf2c5efddb3cb7f33a42655a5f73a8
krbu06001 <krbu06001@E-PW05CGQY>
2024-09-12 14:28:57 +0200

    Add project files.

---
+++ Inlämnings uppgift 1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Uppgift_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Nästa rad gör att det som står i citat täckned står i programmet.
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            //Nästa rad gör samma sak. 
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och\\.");
+        }
+    }
+}
+        
+    
